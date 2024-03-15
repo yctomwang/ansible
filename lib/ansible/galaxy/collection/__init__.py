b5251c24b0c462fb5a9d9b8166928b8ecb9e576b
Thomas Wang <yctomwang123@gmail.com>
2024-03-15 23:48:41 +1100

    work

--- lib/ansible/galaxy/collection/__init__.py
+++ lib/ansible/galaxy/collection/__init__.py
@@ -1307,7 +1307,7 @@ def _build_collection_tar(
         file_manifest,  # type: FilesManifestType
 ):  # type: (...) -> str
     """Build a tar.gz collection artifact from the manifest data."""
-    inner_files = file_manifest["files"]  # type: t.list[t.dict[Literal['name', 'ftype', 'chksum_type', 'chksum_sha256', 'format'], str | int | None]] | int
+    inner_files = file_manifest["files"]  # type: list[t.dict[Literal['name', 'ftype', 'chksum_type', 'chksum_sha256', 'format'], str | int | None]] | int
     inner_files.sort(key=lambda x: x["name"])
     file_manifest["files"] = inner_files
     files_manifest_json = to_bytes(json.dumps(file_manifest, indent=True), errors='surrogate_or_strict')
